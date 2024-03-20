ed4da8895d5941d0fa75c6726d2f41cf926bbc5e
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-03-19 20:25:47 -0400

    3 ways to write loops to create the dynamic nav headers.
    Old School strings vs Template Strings (better)

--- app/js/index.js
+++ app/js/index.js
@@ -2,11 +2,28 @@
 const nav = document.querySelector('.main-menu');
 const navList = document.createElement('ul');
 
-for (let i = 0; i < navItemsArray.length; i++) {
+//3 ways to write loops
+
+//1) First, switch out the old school concatenation for a template string:
+// for (let i = 0; i < navItemsArray.length; i++) {
+//     let listItem = document.createElement('li');
+//     let linkText = navItemsArray[i];
+//     listItem.innerHTML = '<a href="#">' + linkText + '</a>';
+//     navList.append(listItem);
+// }
+
+//2) forEach
+// navItemsArray.forEach(function (item) {
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = `<a href="#">${item}</a>`;
+//     navList.appendChild(listItem);
+// });
+
+//3)Use an arrow function in the final script:
+navItemsArray.forEach(item => {
     let listItem = document.createElement('li');
-    let linkText = navItemsArray[i];
-    listItem.innerHTML = '<a href="#">' + linkText + '</a>';
-    navList.append(listItem);
-}
+    listItem.innerHTML = `<a href="#">${item}</a>`;
+    navList.appendChild(listItem);
+});
 
 nav.append(navList);
